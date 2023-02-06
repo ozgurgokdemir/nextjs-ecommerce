4efb79519847496d423252c97996fdd00afb698a
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-02-07 02:17:38 +0300

    Refactor Spinner from Button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,5 +1,6 @@
 import type { ButtonHTMLAttributes, ComponentType, SVGProps } from 'react';
 import { clsx } from 'clsx';
+import { Spinner } from '@/components/ui';
 
 type ButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'children'> & {
   text: string;
@@ -40,26 +41,7 @@ export default function Button(props: ButtonProps) {
     >
       {isLoading && loadingText ? loadingText : text}
       {isLoading ? (
-        <svg
-          className="h-5 w-5 animate-spin"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="text-slate-600"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            stroke-width="4"
-          ></circle>
-          <path
-            className="text-slate-50"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
+        <Spinner size={size} variant={variant} />
       ) : (
         Icon && <Icon className={size === 'medium' ? 'h-4' : 'h-5'} />
       )}
