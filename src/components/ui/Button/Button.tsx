c00d3a9087dac43b7271f263a5d5bac1a283d1f7
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-02-07 01:24:42 +0300

    Update Button Component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,27 +1,68 @@
-import { ReactNode } from 'react';
+import type { ButtonHTMLAttributes, ComponentType, SVGProps } from 'react';
+import { clsx } from 'clsx';
 
-type ButtonProps = {
+type ButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'children'> & {
   text: string;
   size?: 'medium' | 'large';
-  type?: 'primary' | 'secondary';
-  icon?: ReactNode;
+  variant?: 'primary' | 'secondary';
+  isLoading?: boolean;
+  loadingText?: string;
+  icon?: ComponentType<SVGProps<SVGSVGElement>>;
 };
 
 export default function Button(props: ButtonProps) {
-  const { text, size, type, icon } = props;
+  const {
+    text,
+    size = 'large',
+    variant = 'primary',
+    icon: Icon,
+    type = 'button',
+    disabled,
+    isLoading,
+    loadingText,
+    className,
+    ...restProps
+  } = props;
 
   return (
     <button
-      className={`flex items-center justify-center px-6 gap-2 rounded-lg font-medium ${
-        size === 'medium' ? 'h-9 text-sm' : 'h-12 text-base'
-      } ${
-        type === 'secondary'
-          ? 'text-slate-800 bg-slate-100 hover:bg-slate-50'
-          : 'text-slate-50 bg-slate-800 hover:bg-slate-700'
-      }`}
+      className={clsx(
+        'flex items-center justify-center px-6 rounded-lg font-medium whitespace-nowrap transition',
+        size === 'medium' ? 'h-9 gap-2 text-sm' : 'h-12 gap-3 text-base',
+        variant === 'primary'
+          ? 'text-slate-50 bg-slate-800 hover:bg-slate-700'
+          : 'text-slate-800 bg-slate-100 hover:bg-slate-50',
+        (disabled || isLoading) && 'cursor-not-allowed',
+        className
+      )}
+      type={type}
+      {...restProps}
     >
-      {text}
-      {icon}
+      {isLoading && loadingText ? loadingText : text}
+      {isLoading ? (
+        <svg
+          className="h-5 w-5 animate-spin"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="text-slate-600"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            className="text-slate-50"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      ) : (
+        Icon && <Icon className={size === 'medium' ? 'h-4' : 'h-5'} />
+      )}
     </button>
   );
 }
