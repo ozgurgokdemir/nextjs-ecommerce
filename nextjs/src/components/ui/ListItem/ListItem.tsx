4970507e74322c3750dabbecf628d306fbc49492
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-04-11 03:53:09 +0300

    Update ListItem Component

--- nextjs/src/components/ui/ListItem/ListItem.tsx
+++ nextjs/src/components/ui/ListItem/ListItem.tsx
@@ -1,60 +1,36 @@
-import { ChevronRightIcon } from '@heroicons/react/24/outline';
-import { clsx } from 'clsx';
-import Image from 'next/image';
 import Link from 'next/link';
+import { clsx } from 'clsx';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
 
 type ListItemProps = {
   href: string;
   text: string;
   typeahead?: string;
-  image?: string;
-  imageAlt?: string;
   size?: 'medium' | 'large';
+  className?: string;
 };
 
 export default function ListItem(props: ListItemProps) {
-  const { href, text, typeahead, image, imageAlt, size = 'medium' } = props;
+  const { href, text, typeahead, size = 'medium', className } = props;
 
   return (
-    <li>
+    <li className="shadow-stroke-b bg-white hover:bg-slate-50 transition-colors">
       <Link
-        className={`flex items-center justify-between px-6 bg-white hover:bg-slate-50 border-b border-b-slate-100 transition-colors ${
-          size === 'medium' ? 'h-16' : 'h-24'
-        }`}
+        className={clsx(
+          'flex items-center justify-between px-6',
+          size === 'medium' ? 'h-16' : 'h-20',
+          className
+        )}
         href={href}
       >
         <div
           className={clsx(
             'flex items-center',
-            size === 'medium'
-              ? image
-                ? 'text-label-sm-600'
-                : 'text-label-sm-500'
-              : image
-              ? 'text-label-base-600'
-              : 'text-label-base-500',
-            image && 'gap-6'
+            size === 'medium' ? 'text-label-sm-500' : 'text-label-base-600'
           )}
         >
-          {image && (
-            <Image
-              className={`rounded-lg ${
-                size === 'medium' ? 'w-9 h-9' : 'w-12 h-12'
-              }`}
-              src={image}
-              alt={imageAlt ?? 'list item'}
-              width={size === 'medium' ? 36 : 48}
-              height={size === 'medium' ? 36 : 48}
-            />
-          )}
-          {typeahead ? (
-            <span>
-              {text}
-              <b className="font-bold">{typeahead}</b>
-            </span>
-          ) : (
-            text
-          )}
+          <span>{text}</span>
+          {typeahead && <b className="font-bold">{typeahead}</b>}
         </div>
         <ChevronRightIcon className="h-5 text-slate-400" />
       </Link>
