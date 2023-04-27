709db22b3c151a2937b2f9e94b530b659d966c11
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-04-27 08:50:44 +0300

    Fix "Connection Error: Connection ended unexpectedly"

--- strapi/config/env/production/database.ts
+++ strapi/config/env/production/database.ts
@@ -10,5 +10,15 @@ module.exports = ({ env }) => ({
       password: env("PGPASSWORD", "password"),
       ssl: env.bool(true),
     },
+    acquireConnectionTimeout: 5000,
+    pool: {
+      min: 0,
+      max: 10,
+      createTimeoutMillis: 8000,
+      acquireTimeoutMillis: 8000,
+      idleTimeoutMillis: 8000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+    },
   },
 });
