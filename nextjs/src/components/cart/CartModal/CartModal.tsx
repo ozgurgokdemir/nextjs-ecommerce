0b455758a4c95b9b6ccfacf79ae6aa381885c765
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-05-10 22:02:27 +0300

    Add Remove Cart Item Animation

--- nextjs/src/components/cart/CartModal/CartModal.tsx
+++ nextjs/src/components/cart/CartModal/CartModal.tsx
@@ -1,9 +1,34 @@
-import { Fragment } from 'react';
 import { CreditCardIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import { Dialog } from '@headlessui/react';
+import { AnimatePresence, motion } from 'framer-motion';
 import { Button, Modal } from '@/components/ui';
 import { CartItem } from '@/components/cart';
 import { useCartStore } from '@/lib/store';
+
+const variants = {
+  cartContent: {
+    hidden: {
+      opacity: 0,
+      transition: { ease: 'easeIn', duration: 0.2 },
+    },
+    show: {
+      opacity: 1,
+      transition: { ease: 'easeOut', duration: 0.3 },
+    },
+  },
+  cartItem: {
+    hidden: {
+      height: 0,
+      opacity: 0,
+      transition: { ease: 'easeIn', duration: 0.2 },
+    },
+    show: {
+      height: 'auto',
+      opacity: 1,
+      transition: { ease: 'easeOut', duration: 0.3 },
+    },
+  },
+};
 
 export default function CartModal() {
   const { isCartOpen, closeCart, cartItems, totalPrice } = useCartStore();
@@ -14,54 +39,82 @@ export default function CartModal() {
       onClose={closeCart}
       className="w-full max-w-[30rem] flex flex-col"
     >
-      {cartItems.length > 0 ? (
-        <Fragment>
-          <ul className="pt-6 flex flex-col max-h-[28.5rem] overflow-y-auto">
-            {cartItems.map((item) => (
-              <li className="shadow-stroke-b last:shadow-none" key={item.id}>
-                <CartItem item={item} />
-              </li>
-            ))}
-          </ul>
-          <div className="p-12 pt-6 flex flex-col gap-6">
-            <div className="flex items-center justify-between">
-              <span className="text-label-base-600 text-slate-400">TOTAL</span>
-              <span className="font-secondary text-label-xl-600">
-                ${totalPrice}
-              </span>
+      <AnimatePresence initial={false} mode="popLayout">
+        {cartItems.length > 0 ? (
+          <motion.div
+            key="cart"
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            variants={variants.cartContent}
+          >
+            <ul className="pt-6 flex flex-col max-h-[28.5rem] overflow-x-hidden overflow-y-auto">
+              <AnimatePresence initial={false}>
+                {cartItems.map((item) => (
+                  <motion.li
+                    key={item.id}
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                    variants={variants.cartItem}
+                    className="shadow-stroke-b last:shadow-none"
+                  >
+                    <div className="h-full max-h-fit overflow-hidden">
+                      <CartItem item={item} />
+                    </div>
+                  </motion.li>
+                ))}
+              </AnimatePresence>
+            </ul>
+            <div className="p-12 pt-6 flex flex-col gap-6">
+              <div className="flex items-center justify-between">
+                <span className="text-label-base-600 text-slate-400">
+                  TOTAL
+                </span>
+                <span className="font-secondary text-label-xl-600">
+                  ${totalPrice}
+                </span>
+              </div>
+              <div className="flex gap-6">
+                <Button
+                  className="flex-1"
+                  text="Close"
+                  variant="secondary"
+                  onClick={closeCart}
+                />
+                <Button
+                  className="flex-1"
+                  text="Checkout"
+                  icon={CreditCardIcon}
+                />
+              </div>
             </div>
-            <div className="flex gap-6">
-              <Button
-                className="flex-1"
-                text="Close"
-                variant="secondary"
-                onClick={closeCart}
-              />
-              <Button
-                className="flex-1"
-                text="Checkout"
-                icon={CreditCardIcon}
-              />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="cart-empty"
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            variants={variants.cartContent}
+            className="p-12 relative flex flex-col gap-4"
+          >
+            <div className="p-6 absolute top-0 right-0">
+              <button type="button" onClick={closeCart}>
+                <XMarkIcon className="h-6" />
+              </button>
             </div>
-          </div>
-        </Fragment>
-      ) : (
-        <div className="p-12 relative flex flex-col gap-4">
-          <div className="p-6 absolute top-0 right-0">
-            <button type="button" onClick={closeCart}>
-              <XMarkIcon className="h-6" />
-            </button>
-          </div>
-          <div className="w-36 h-36 mx-auto mb-4 bg-slate-100 rounded-lg"></div>
-          <Dialog.Title className="font-secondary text-heading-2xl">
-            Your Shopping Cart is Empty
-          </Dialog.Title>
-          <Dialog.Description className="text-body-base-400">
-            Browse our products to find something you need, or check out our
-            latest deals and offers.
-          </Dialog.Description>
-        </div>
-      )}
+            <div className="w-36 h-36 mx-auto mb-4 bg-slate-100 rounded-lg"></div>
+            <Dialog.Title className="font-secondary text-heading-2xl">
+              Your Shopping Cart is Empty
+            </Dialog.Title>
+            <Dialog.Description className="text-body-base-400">
+              Browse our products to find something you need, or check out our
+              latest deals and offers.
+            </Dialog.Description>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </Modal>
   );
 }
--- nextjs/src/pages/cart/index.tsx
+++ nextjs/src/pages/cart/index.tsx
@@ -1,35 +1,83 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { CartLayout } from '@/components/layout';
 import { CartItem } from '@/components/cart';
 import { useCartStore } from '@/lib/store';
 
+const variants = {
+  cartContent: {
+    hidden: {
+      opacity: 0,
+      transition: { ease: 'easeIn', duration: 0.2 },
+    },
+    show: {
+      opacity: 1,
+      transition: { ease: 'easeOut', duration: 0.3 },
+    },
+  },
+  cartItem: {
+    hidden: {
+      height: 0,
+      opacity: 0,
+      transition: { ease: 'easeIn', duration: 0.2 },
+    },
+    show: {
+      height: 'auto',
+      opacity: 1,
+      transition: { ease: 'easeOut', duration: 0.3 },
+    },
+  },
+};
+
 export default function Cart() {
   const { cartItems } = useCartStore();
 
-  if (cartItems.length === 0) {
-    return (
-      <section className="px-6 flex-1 flex flex-col items-center justify-center gap-4 text-center">
-        <div className="w-36 h-36 mb-4 bg-slate-100 rounded-lg"></div>
-        <h1 className="font-secondary text-heading-2xl">
-          Your Shopping Cart <br /> is Empty
-        </h1>
-        <p className="text-body-sm-400 text-slate-600">
-          Browse our products to find something you need, or check out our
-          latest deals and offers.
-        </p>
-      </section>
-    );
-  }
-
   return (
-    <section>
-      <ul className="flex flex-col">
-        {cartItems.map((item) => (
-          <li className="shadow-stroke-b last:shadow-none" key={item.id}>
-            <CartItem item={item} />
-          </li>
-        ))}
-      </ul>
-    </section>
+    <AnimatePresence initial={false} mode="popLayout">
+      {cartItems.length > 0 ? (
+        <motion.section
+          key="cart"
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          variants={variants.cartContent}
+        >
+          <ul className="flex flex-col">
+            <AnimatePresence initial={false}>
+              {cartItems.map((item) => (
+                <motion.li
+                  key={item.id}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  variants={variants.cartItem}
+                  className="shadow-stroke-b last:shadow-none overflow-hidden"
+                >
+                  <CartItem item={item} />
+                </motion.li>
+              ))}
+            </AnimatePresence>
+          </ul>
+        </motion.section>
+      ) : (
+        <motion.section
+          key="cart-empty"
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          variants={variants.cartContent}
+          className="px-6 flex-1 flex flex-col items-center justify-center gap-4 text-center"
+        >
+          <div className="w-36 h-36 mb-4 bg-slate-100 rounded-lg"></div>
+          <h1 className="font-secondary text-heading-2xl">
+            Your Shopping Cart <br /> is Empty
+          </h1>
+          <p className="text-body-sm-400 text-slate-600">
+            Browse our products to find something you need, or check out our
+            latest deals and offers.
+          </p>
+        </motion.section>
+      )}
+    </AnimatePresence>
   );
 }
 
