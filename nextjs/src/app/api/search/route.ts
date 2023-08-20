8f7f380be74d44d3b31c194c8e082317b4a26a72
Özgür Gökdemir <ozgurgokd@gmail.com>
2023-08-21 02:33:16 +0300

    refactor: Migrate the Search API to the App Router

--- nextjs/src/app/api/search/route.ts
+++ nextjs/src/app/api/search/route.ts
@@ -1,25 +1,22 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse, type NextRequest } from 'next/server';
 import { strapi } from '@/lib/api';
 
-type ResponseData = Awaited<ReturnType<typeof strapi.getSearchResults>>;
+type ResponseData =
+  | Awaited<ReturnType<typeof strapi.getSearchResults>>
+  | { error: Error['message'] };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData | Error['message']>
-) {
-  if (req.method !== 'POST') return;
-
+export async function POST(request: NextRequest) {
   try {
-    const searchQuery = req.body as string;
+    const searchQuery = (await request.json()) as string;
 
     const searchResults = await strapi.getSearchResults(searchQuery);
 
-    res.status(200).json(searchResults);
+    return NextResponse.json<ResponseData>(searchResults, { status: 200 });
   } catch (error) {
     const { message } = error as Error;
 
     console.error(message);
 
-    res.status(500).json(message);
+    return NextResponse.json<ResponseData>({ error: message }, { status: 500 });
   }
 }
